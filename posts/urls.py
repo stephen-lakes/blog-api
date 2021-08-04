8f66a1ebd84023cb5a59dd411d9e4b62c6353a72
stephen-lakes <oluyomiolamilekan99@gmail.com>
2021-08-04 19:20:09 +0100

    added routers, removed generic views

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
 
-from .views import PostList, PostDetail
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
 
-urlpatterns = [
-    path('/<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+
+urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,19 @@
 #from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from django.db.models import query
-from rest_framework import generics, permissions
+from rest_framework import viewsets
 from .models import Post
 from .permissions import IsAuthorOrReadOnly # new
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    #permission_classes = (permissions.IsAuthenticated,)
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset           = Post.objects.all()
     serializer_class   = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,) # new
     queryset           = Post.objects.all()
     serializer_class   = PostSerializer
